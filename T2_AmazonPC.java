3df6464101dcedbc917d20c76792e68985f5407c
Neelima Nuti <neelima.nuti880@gmail.com>
2020-06-20 21:18:22 -0400

    V.1 of all 3 Amazon_task

---
+++ T2_AmazonPC.java
@@ -0,0 +1,18 @@
+package Task2;
+
+
+public class T2_AmazonPC {
+	
+	public static String searchbartab = "#twotabsearchtextbox";
+	public static String toCart = "#add-to-cart-button";
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
